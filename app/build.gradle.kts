bc40d2f8992d75e699a3825113d2db414cb05a19
Cam Hashemi <camhashemi@gmail.com>
2021-10-13 10:45:42 +0200

    project init

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "1.5.30"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    mainClass.set("kotlin.grpc.AppKt")
+}
---
+++ app/src/main/kotlin/sample/grpc/App.kt
@@ -0,0 +1,12 @@
+package sample.grpc
+
+class App {
+    val greeting: String
+        get() {
+            return "Hello World!"
+        }
+}
+
+fun main() {
+    println(App().greeting)
+}
