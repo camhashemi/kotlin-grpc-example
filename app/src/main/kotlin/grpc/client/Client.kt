1bf223a5b014769d65652b53d161b9f480b4e80e
Cam Hashemi <camhashemi@gmail.com>
2021-10-16 12:10:17 +0200

    client: send many concurrent requests

--- app/src/main/kotlin/grpc/client/Client.kt
+++ app/src/main/kotlin/grpc/client/Client.kt
@@ -1,9 +1,15 @@
 package grpc.client
 
+import grpc.logger.Logger
 import grpc.proto.Server
 import grpc.proto.UserServiceGrpcKt
 import io.grpc.ManagedChannelBuilder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import java.util.*
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTime
 
 fun main() {
     val channel =
@@ -15,13 +21,39 @@ fun main() {
     val client = UserServiceGrpcKt.UserServiceCoroutineStub(channel)
 
     runBlocking {
-        val response = client.saveUser(
-            Server.SaveUserRequest.newBuilder()
-                .setEmail("a@b.c")
-                .build()
-        )
+        try {
+            client.createManyUsers()
+        }catch (e: Exception) {
+            Logger.error(e.cause)
+        }
+    }
+}
 
-        println("INFO: created user ${response.id}")
+@OptIn(ExperimentalTime::class)
+fun UserServiceGrpcKt.UserServiceCoroutineStub.createManyUsers() {
+    val n = 10_000
+    measureTime {
+        runBlocking {
+            sendSaveUserConcurrent(this, n)
+        }
+    }.also { println("created $n users in ${it.inWholeMilliseconds}s") }
+}
+
+suspend fun UserServiceGrpcKt.UserServiceCoroutineStub.sendSaveUser(email: String) {
+    val response = saveUser(
+        Server.SaveUserRequest.newBuilder()
+            .setEmail(email)
+            .build()
+    )
+
+    Logger.debug("created user ${response.id}")
+}
+
+suspend fun UserServiceGrpcKt.UserServiceCoroutineStub.sendSaveUserConcurrent(scope: CoroutineScope, n: Int) {
+    repeat(n) { i ->
+        scope.launch {
+            sendSaveUser(UUID.randomUUID().toString())
+        }
     }
 }
 
