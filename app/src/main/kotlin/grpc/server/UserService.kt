a706ad99b5c04af9c639b7bfabfd31591f614f38
Cam Hashemi <camhashemi@gmail.com>
2021-10-16 12:12:58 +0200

    server: make SaveUser thread-safe

--- app/src/main/kotlin/grpc/server/UserService.kt
+++ app/src/main/kotlin/grpc/server/UserService.kt
@@ -2,14 +2,33 @@ package grpc.server
 
 import grpc.proto.Server
 import grpc.proto.UserServiceGrpcKt
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
-class UserService: UserServiceGrpcKt.UserServiceCoroutineImplBase() {
+typealias ID = String
+typealias Email = String
+
+data class User(val id: ID, val email: Email)
+
+class UserService : UserServiceGrpcKt.UserServiceCoroutineImplBase() {
 
     private var currentID = 0
+    private val users = mutableListOf<User>()
+    private val lock = Mutex()
 
     override suspend fun saveUser(request: Server.SaveUserRequest): Server.SaveUserResponse {
-        return Server.SaveUserResponse.newBuilder()
-            .setId(currentID++.toString())
-            .build()
+        lock.withLock {
+            if (users.any { it.email == request.email }) {
+                throw Exception("user with email already exists")
+            }
+
+            val id = currentID++
+
+            users.add(User(id.toString(), request.email))
+
+            return Server.SaveUserResponse.newBuilder()
+                .setId(id.toString())
+                .build()
+        }
     }
 }
