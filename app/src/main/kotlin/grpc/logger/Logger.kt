a342203fa0dbc1c72a25ed1485a234b5faa260ec
Cam Hashemi <camhashemi@gmail.com>
2021-10-16 12:10:33 +0200

    logger: add error/debug levels

--- app/src/main/kotlin/grpc/logger/Logger.kt
+++ app/src/main/kotlin/grpc/logger/Logger.kt
@@ -1,7 +1,18 @@
 package grpc.logger
 
 object Logger {
-    fun info(msg: Any) {
+
+    private const val isDebugEnabled = false
+
+    fun info(msg: Any?) {
         println("INFO: $msg")
+    }
+
+    fun error(msg: Any?) {
+        println("ERROR: $msg")
+    }
+
+    fun debug(msg: Any) {
+        if (isDebugEnabled) println("DEBUG: $msg")
     }
 }
